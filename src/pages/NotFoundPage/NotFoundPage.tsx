1f9d57776cf3b6f0fa23306165059709985d8976
Aleksei P <civil_a@list.ru>
2022-11-19 01:36:38 +0300

    feat: add task-component

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,19 +1,7 @@
-import { Card, Modal } from 'antd';
-import { Column } from 'components';
 import React, { useState } from 'react';
-// import style from './NotFoundPage.module.scss';
 
 const NotFoundPage = () => {
-  const [tasks, setTask] = useState([1, 2, 3, 4, 5, 6]);
-  return (
-    <Modal open={true}>
-      <Column title="task">
-        {tasks.map((task) => (
-          <Card key={task}>{task}</Card>
-        ))}
-      </Column>
-    </Modal>
-  );
+  return <div>404</div>;
 };
 
 export default NotFoundPage;
