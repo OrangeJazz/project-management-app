df8ef6e6cf6458bd28f3853abe2dc78956e92292
OrangeJazz <wilinwen@mail.ru>
2022-11-20 03:41:03 +0400

    add: search logic to boards page

--- src/pages/BoardsPage/BoardsPage.tsx
+++ src/pages/BoardsPage/BoardsPage.tsx
@@ -1,26 +1,33 @@
 import { BoardsContainer, SearchBar } from 'components';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './BoardsPage.module.scss';
-import { useAppDispatch } from '../../hooks';
+import { useAppDispatch, useAppSelector } from '../../hooks';
 import { getUserBoards } from '../../store/sliceBoards';
 
 const BoardsPage = () => {
+  const boardsState = useAppSelector((state) => state.boards);
   const dispatch = useAppDispatch();
+  const [filteredBoards, setFilteredBoards] = useState([...boardsState.boards]);
 
   useEffect(() => {
     dispatch(getUserBoards());
   }, [dispatch]);
 
-  const getSearchValue = (value: string) => {
-    console.log(value);
+  const searchValue = (value: string) => {
+    if (!value) {
+      setFilteredBoards([...boardsState.boards]);
+      return;
+    }
+    const filteredBoards = boardsState.boards.filter((el) => el.title.includes(value));
+    setFilteredBoards(filteredBoards);
   };
 
   return (
     <div className={styles['boards__wrapper']}>
       <div className={styles['boards__content']}>
         <h2 className={styles['boards__heading']}>Your Projects:</h2>
-        <SearchBar searchValue={getSearchValue} />
-        <BoardsContainer />
+        <SearchBar searchValue={searchValue} />
+        <BoardsContainer boards={filteredBoards} />
       </div>
     </div>
   );
