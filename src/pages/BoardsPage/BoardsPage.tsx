dc441009e6c90521f00f31c3492efaa09da4c597
OrangeJazz <wilinwen@mail.ru>
2022-11-20 03:08:21 +0400

    add: search bar component

--- src/pages/BoardsPage/BoardsPage.tsx
+++ src/pages/BoardsPage/BoardsPage.tsx
@@ -6,14 +6,20 @@ import { getUserBoards } from '../../store/sliceBoards';
 
 const BoardsPage = () => {
   const dispatch = useAppDispatch();
+
   useEffect(() => {
     dispatch(getUserBoards());
   }, [dispatch]);
+
+  const getSearchValue = (value: string) => {
+    console.log(value);
+  };
+
   return (
     <div className={styles['boards__wrapper']}>
       <div className={styles['boards__content']}>
         <h2 className={styles['boards__heading']}>Your Projects:</h2>
-        <SearchBar />
+        <SearchBar searchValue={getSearchValue} />
         <BoardsContainer />
       </div>
     </div>
