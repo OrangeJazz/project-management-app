6e2aca8c88afaa175d1deeedd62bad5e1d3e2be5
OrangeJazz <wilinwen@mail.ru>
2022-11-23 02:59:22 +0400

    add: main card component, problems with styles

--- src/components/AboutUs/AboutUs.tsx
+++ src/components/AboutUs/AboutUs.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './AboutUs.module.scss';
 import { MainCard } from 'components';
+import { ABOUT_CARDS } from '../../store/data';
 
 const AboutUs = () => {
   return (
@@ -8,9 +9,27 @@ const AboutUs = () => {
       <div className={styles.about__container}>
         <h3 className={styles.about__heading}>Our Team</h3>
         <div className={styles.about__cards}>
-          <MainCard />
-          <MainCard />
-          <MainCard />
+          <MainCard
+            type="about"
+            title={ABOUT_CARDS[0].title}
+            subtitle={ABOUT_CARDS[0].subtitle}
+            text={ABOUT_CARDS[0].text}
+            img={ABOUT_CARDS[0].img}
+          />
+          <MainCard
+            type="about"
+            title={ABOUT_CARDS[1].title}
+            subtitle={ABOUT_CARDS[1].subtitle}
+            text={ABOUT_CARDS[1].text}
+            img={ABOUT_CARDS[1].img}
+          />
+          <MainCard
+            type="about"
+            title={ABOUT_CARDS[2].title}
+            subtitle={ABOUT_CARDS[2].subtitle}
+            text={ABOUT_CARDS[2].text}
+            img={ABOUT_CARDS[2].img}
+          />
         </div>
       </div>
     </div>
---
+++ src/store/data.ts
@@ -0,0 +1,45 @@
+import features1 from '../assets/img/features1.svg';
+import features2 from '../assets/img/features2.svg';
+import features3 from '../assets/img/features3.svg';
+// import about2 from '../../public/about2.jpg';
+
+const FEATURES_CARDS = [
+  {
+    title: 'Automating Daily Tasks from Your Phone',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa nostrum voluptates hic iure tenetur ducimus. Quos pariatur id, fugiat quam cupiditate architecto odit a eveniet odio obcaecati voluptate earum excepturi.',
+    img: features1,
+  },
+  {
+    title: 'Automating Daily Tasks from Your Phone',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa nostrum voluptates hic iure tenetur ducimus. Quos pariatur id, fugiat quam cupiditate architecto odit a eveniet odio obcaecati voluptate earum excepturi.',
+    img: features2,
+  },
+  {
+    title: 'Automating Daily Tasks from Your Phone',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa nostrum voluptates hic iure tenetur ducimus. Quos pariatur id, fugiat quam cupiditate architecto odit a eveniet odio obcaecati voluptate earum excepturi.',
+    img: features3,
+  },
+];
+
+const ABOUT_CARDS = [
+  {
+    title: 'Alexey Pepelyaev',
+    subtitle: 'Team Lead',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa nostrum voluptates hic iure tenetur ducimus. Quos pariatur id, fugiat quam cupiditate architecto odit a eveniet odio obcaecati voluptate earum excepturi.',
+    img: 'about1.png',
+  },
+  {
+    title: 'Mariya Vasileva',
+    subtitle: 'Developer',
+    text: 'Automating Daily Tasks from Your Phone',
+    img: 'about2.jpg',
+  },
+  {
+    title: 'Alexandr',
+    subtitle: 'Developer',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa nostrum voluptates hic iure tenetur ducimus. Quos pariatur id, fugiat quam cupiditate architecto odit a eveniet odio obcaecati voluptate earum excepturi.',
+    img: 'about3.png',
+  },
+];
+
+export { FEATURES_CARDS, ABOUT_CARDS };
